cd2b7d7488ac74053e7100275fdd6bf29655be73
Иван Гриб <isgb194@gmail.com>
2025-04-10 14:12:39 +0300

    feat: implemented GetSessionAsync() and GetSessionsAsync() methods

--- Lab5TestTask/Services/Implementations/SessionService.cs
+++ Lab5TestTask/Services/Implementations/SessionService.cs
@@ -1,4 +1,5 @@
 ﻿using Lab5TestTask.Data;
+using Lab5TestTask.Enums;
 using Lab5TestTask.Models;
 using Lab5TestTask.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
@@ -20,11 +21,17 @@ public class SessionService : ISessionService
 
     public async Task<Session> GetSessionAsync()
     {
-        throw new NotImplementedException();
+        return await _dbContext.Sessions
+            .Where(s => s.DeviceType == DeviceType.Desktop)
+            .OrderBy(s => s.StartedAtUTC)
+            .FirstOrDefaultAsync();
     }
 
     public async Task<List<Session>> GetSessionsAsync()
     {
-        throw new NotImplementedException();
+        var restrictionDate = new DateTime(2025, 1, 1);
+        return await _dbContext.Sessions
+            .Where(s => s.User.Status == UserStatus.Active && s.EndedAtUTC < restrictionDate)
+            .ToListAsync();
     }
 }
